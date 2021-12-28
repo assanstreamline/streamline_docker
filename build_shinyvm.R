01d14a65f549d06a9c9630f9b6bca8799d353ffc
John Muschelli <muschellij2@gmail.com>
2021-12-28 15:36:50 +0000

    added dev streamliner

--- build_shinyvm.R
+++ build_shinyvm.R
@@ -1,18 +1,8 @@
 library(googleCloudRunner)
 library(trailrun)
-source("cr_helpers.R")
 setup = cr_gce_setup()
 options("googleAuthR.verbose" = 3)
 image = "us-docker.pkg.dev/streamline-resources/streamline-private-repo/streamliner-packages"
-pre_steps = c(
-  cr_buildstep_docker_auth_location(c("us-east4", "us")),
-  cr_buildstep(
-    id = "My docker pull",
-    name = "docker",
-    args = c("pull",
-             image)
-  )
-)
 
 
 result = cr_deploy_docker(
