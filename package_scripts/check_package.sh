a0d50ddcbbb5f1e9e3d5c4631b4aaeb837a88a1a
John Muschelli <muschellij2@gmail.com>
2022-02-17 17:03:14 +0000

    added deps sh

--- package_scripts/check_package.sh
+++ package_scripts/check_package.sh
@@ -1,3 +1,4 @@
+R -f /package_scripts/isntall_deps.R
 package_name=`cat DESCRIPTION | grep Package: | awk '{print $2}'` && \
 R -f /package_scripts/check_package.R || exit_code=$?
 tar xzf tar cvzf ${package_name}_check.tar.gz check/ 
