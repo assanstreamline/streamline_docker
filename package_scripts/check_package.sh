a7b9a883da16f79010cb70e101e321bd536f2347
John Muschelli <muschellij2@gmail.com>
2022-02-17 17:14:42 +0000

    adding in check_package

--- package_scripts/check_package.sh
+++ package_scripts/check_package.sh
@@ -1,3 +1,8 @@
+echo "ls"
+ls -l
+echo "ls workspace"
+ls -l /workspace || true
+echo "PWD is ${PWD}"
 R -f /package_scripts/isntall_deps.R
 package_name=`cat DESCRIPTION | grep Package: | awk '{print $2}'` && \
 R -f /package_scripts/check_package.R || exit_code=$?
