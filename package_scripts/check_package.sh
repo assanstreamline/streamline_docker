58b67d97e84d1da305b6d49265900d5d53924ecb
John Muschelli <muschellij2@gmail.com>
2022-02-17 19:36:46 +0000

    stupid typo on sh file

--- package_scripts/check_package.sh
+++ package_scripts/check_package.sh
@@ -3,7 +3,7 @@ ls -l
 echo "ls workspace"
 ls -l /workspace || true
 echo "PWD is ${PWD}"
-R -f /package_scripts/isntall_deps.R
+R -f /package_scripts/install_deps.R
 package_name=`cat DESCRIPTION | grep Package: | awk '{print $2}'` && \
 R -f /package_scripts/check_package.R || exit_code=$?
 
