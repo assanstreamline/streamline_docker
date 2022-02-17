9f5363d21c2611fc2cff084cfe1216eea7b53126
John Muschelli <muschellij2@gmail.com>
2022-02-17 16:33:18 +0000

    fixed typo in sh

--- package_scripts/check_package.sh
+++ package_scripts/check_package.sh
@@ -1,5 +1,4 @@
 package_name=`cat DESCRIPTION | grep Package: | awk '{print $2}'` && \
-R /package_scripts/check_package.R || exit_code=$?
-
+R -f /package_scripts/check_package.R || exit_code=$?
 tar xzf tar cvzf ${package_name}_check.tar.gz check/ 
 exit ${exit_code}
