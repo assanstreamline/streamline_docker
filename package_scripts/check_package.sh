f7bd3b248a8876b00702f60c91df95ab5778fb9c
John Muschelli <muschellij2@gmail.com>
2022-02-22 16:40:07 +0000

    added another ls and zipping the subcheck

--- package_scripts/check_package.sh
+++ package_scripts/check_package.sh
@@ -41,17 +41,18 @@ package_name=`cat DESCRIPTION | grep Package: | awk '{print $2}'` && \
 echo "package_name is ${package_name}" && \
 R -f /package_scripts/check_package.R || exit_code=$?
 
+echo ${exit_code} > /workspace/exit_code.txt
 
-echo "ls"
-ls -la
+ls -la /workspace || true
+tar cvzf /workspace/${package_name}_check.tar.gz check/*.Rcheck || true
+
 echo "ls workspace"
 ls -la /workspace || true
-tar cvzf /workspace/${package_name}_check.tar.gz check/ || true
 
-# Need to extract bucket name or pass this through
-if [[ ${exit_code} -ne 0 ]] && [[ -n "${GCS_DEFAULT_BUCKET}" ]]; then
-  suffix=`date +"%Y-%m-%d"`
-  gsutil cp /workspace/${package_name}_check.tar.gz gs://${bucket_name}/${package_name}_check_${suffix}.tar.gz || true
-fi
-exit ${exit_code}
+# # Need to extract bucket name or pass this through
+# if [[ ${exit_code} -ne 0 ]] && [[ -n "${GCS_DEFAULT_BUCKET}" ]]; then
+#   suffix=`date +"%Y-%m-%d"`
+#   gsutil cp /workspace/${package_name}_check.tar.gz gs://${GCS_DEFAULT_BUCKET}/${package_name}_check_${suffix}.tar.gz || true
+# fi
+# exit ${exit_code}
 
